0ac53d740cb74244d5e16b9d732e8ff8f1457959
Sarthak Kaingade <sarthakkaingade@yahoo.com>
2015-10-09 21:19:44 -0400

    Controlling 2 DC Motors

--- DC_MOTOR_CONTROL/DC_MOTOR_CONTROL.ino
+++ DC_MOTOR_CONTROL/DC_MOTOR_CONTROL.ino
@@ -1,22 +1,29 @@
 /**PerFoRo DC Motor Control Module ***/
 
 const int PWM1 =  23;
-const int INA = 14;
-const int INB = 15;
+const int IN1A = 14;
+const int IN1B = 15;
+const int PWM2 =  22;
+const int IN2A = 16;
+const int IN2B = 17;
 
 void setup()   {                
   pinMode(PWM1, OUTPUT);
-  pinMode(INA, OUTPUT);
-  pinMode(INB, OUTPUT);
+  pinMode(IN1A, OUTPUT);
+  pinMode(IN1B, OUTPUT);
+  pinMode(PWM2, OUTPUT);
+  pinMode(IN2A, OUTPUT);
+  pinMode(IN2B, OUTPUT);
   analogWriteResolution(10);
 }
 
 void loop()                     
 {
   analogWrite(PWM1, 250);
+  digitalWrite(IN1A, HIGH);
+  digitalWrite(IN1B, LOW);
+  analogWrite(PWM2, 250);
+  digitalWrite(IN2A, HIGH);
+  digitalWrite(IN2B, LOW);
   delay(500);
-  digitalWrite(INA, HIGH);
-  delay(100);
-  digitalWrite(INB, LOW);
-  delay(100);
 }
